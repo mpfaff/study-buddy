761f2d80343e4658f4d539a837f7eb07ae190d07
Michael Pfaff <michael@pfaff.dev>
2026-01-17 22:51:18 -0500

    Render flashcards one at a time as the questions are received

--- gemini.mjs
+++ gemini.mjs
@@ -9,17 +9,75 @@ const askResult = document.getElementById("ask-result");
 askProgress.style.display = null;
 askProgress.remove();
 
+const JSON_CODE_FENCE_PREFIX = "```json";
+const JSON_CODE_FENCE_SUFFIX = "```";
+
+function parseIncrementalJson(answer) {
+  if (!answer.startsWith(JSON_CODE_FENCE_PREFIX)) {
+    return [];
+  }
+  answer = answer.substring(JSON_CODE_FENCE_PREFIX.length);
+  if (answer.endsWith(JSON_CODE_FENCE_SUFFIX)) {
+    answer = answer.substring(0, answer.length - JSON_CODE_FENCE_SUFFIX.length);
+  } else {
+    answer = answer.trimEnd();
+    if (answer.endsWith("}")) {
+      answer += ']';
+    } else if (answer.endsWith("},")) {
+      answer = answer.substring(0, answer.length - 1) + ']';
+    }
+  }
+  console.log(answer, JSON.stringify(answer));
+  return JSON.parse(answer);
+}
+
+function renderJsonQuestionSet(set, first) {
+  for (let i = first; i < set.length; i++) {
+    const entry = set[i];
+
+    const el = document.createElement("div");
+    el.classList.add("flashcard");
+
+    const question = document.createElement("div");
+    question.classList.add("flashcard--question");
+    question.innerText = entry.question;
+
+    const answer = document.createElement("div");
+    answer.classList.add("flashcard--answer");
+    answer.innerText = entry.answer;
+
+    el.appendChild(question);
+    el.appendChild(answer);
+    askResult.appendChild(el);
+  }
+}
+
+/**
+ * @type {AbortSignal}
+ */
+let queryAbortController;
+
 askForm.addEventListener('submit', async (e) => {
   e.preventDefault();
+
+  queryAbortController?.abort();
+  queryAbortController = new AbortController();
+  const abortSignal = queryAbortController.signal;
 
   ask.insertBefore(askProgress, askResult);
 
   let answer = '';
   try {
-    const answerStream = askAI([
+    const answerStream = askAI(abortSignal, [
       {
        // text: `The user wants to learn about a topic. Please begin by summarizing the topic in up to 20 words. Answer the question directly, without any "Here is the answer" text.`,
-       text: `The user wants to learn about a topic. Please begin by generating a simple list of questions, one per line, without list formatting. Answer the question directly, without any "Here is the answer" text.`,
+       // text: `The user wants to learn about a topic. Please begin by generating a simple list of questions, one per line, without list formatting. Answer the question directly, without any "Here is the answer" text.`,
+       text: `The user wants to learn about a topic. Please begin by generating a simple list of questions and answers in JSON following this format:
+       \`\`\`
+       [{"question": QUESTION, "answer": ANSWER}, ...]
+       \`\`\`
+
+       Be sure to limit your answers to at most 20 words each.`,
       },
       {
        text: "The topic is:",
@@ -37,18 +95,45 @@ askForm.addEventListener('submit', async (e) => {
        // - https://journals.asm.org/doi/pdf/10.1128/ec.00178-07`,
       },
     ]);
+    askResult.replaceChildren();
+    let renderedCount = 0;
     for await (const chunk of answerStream) {
+      abortSignal.throwIfAborted();
       answer += chunk;
-      askResult.innerText = answer;
+      // askResult.innerText = answer;
+      try {
+        const parsed = parseIncrementalJson(answer);
+        renderJsonQuestionSet(parsed, renderedCount);
+        renderedCount = parsed.length;
+        delete askResult.dataset.error;
+      } catch (e) {
+        console.log(e, answer);
+      }
     }
+    abortSignal.throwIfAborted();
+    // if (!answer.startsWith(JSON_CODE_FENCE_PREFIX) || !answer.endsWith(JSON_CODE_FENCE_SUFFIX)) {
+    //   throw new Error("Invalid JSON response from model");
+    // }
+    // const parsed = parseIncrementalJson(answer);
+    // renderJsonQuestionSet(parsed);
+    delete askResult.dataset.error;
   } catch (e) {
+    if (e instanceof AbortError) return;
+    askResult.dataset.error = '';
     askResult.innerText = e.toString();
   } finally {
     askProgress.remove();
   }
 });
 
-async function* askAI(parts) {
+askQuery.addEventListener('input', (e) => {
+  if (e.target.value == '') {
+    queryAbortController?.abort();
+    askResult.replaceChildren();
+  }
+})
+
+async function* askAI(abortSignal, parts) {
   // const resp = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:streamGenerateContent?alt=sse", {
   const resp = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemma-3-27b-it:streamGenerateContent?alt=sse", {
     method: "POST",
@@ -117,6 +202,7 @@ async function* askAI(parts) {
     //   }
     // }
    }),
+   signal: abortSignal,
   });
   const reader = resp.body
     .pipeThrough(new TextDecoderStream())
@@ -131,7 +217,7 @@ async function* askAI(parts) {
         if (!content.parts) continue;
         yield content.parts[0].text;
       } catch (e) {
-        console.error(e, value);
+        console.debug(e, value);
       }
     }
   }
